4149eb2fa6952552f8c7e0982d0c53c5c5cfa3df
rtran99 <ronytran@live.com>
2021-12-02 12:08:38 -0800

    Revert "Revert "push""
    This reverts commit 67651878fb73c791f16e474464472670523c6459.

--- audit_log/app.py
+++ audit_log/app.py
@@ -7,7 +7,7 @@ import yaml
 import requests
 import json
 from pykafka import KafkaClient
-from  pykafka.common import OffsetType
+from pykafka.common import OffsetType
 from threading import Thread
 logger = logging.getLogger('basicLogger')
 
@@ -19,57 +19,68 @@ with open('log_conf.yml', 'r') as f:
     logging.config.dictConfig(log_config)
     logger = logging.getLogger('basicLogger')
 
+
 def get_search_Inventory(index):
-    hostname = "%s:%d" % (app_config["events"]["hostname"],app_config["events"]["port"])
+    hostname = "%s:%d" % (
+        app_config["events"]["hostname"], app_config["events"]["port"])
     client = KafkaClient(hosts=hostname)
     topic = client.topics[str.encode(app_config["events"]["topic"])]
     # Here we reset the offset on start so that we retrieve
-    # messages at the beginning of the message queue. 
+    # messages at the beginning of the message queue.
     # To prevent the for loop from blocking, we set the timeout to
     # 100ms. There is a risk that this loop never stops if the
     # index is large and messages are constantly being received!
-    consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
+    consumer = topic.get_simple_consumer(
+        reset_offset_on_start=True, consumer_timeout_ms=1000)
     logger.info("Retrieving BP at index %d" % index)
+    counter = 0
     try:
         for msg in consumer:
             msg_str = msg.value.decode('utf-8')
             msg = json.loads(msg_str)
-    # Find the event at the index you want and 
+            if counter == index and msg['type'] == "InventoryItem":
+                return msg, 200
+            counter += 1
+    # Find the event at the index you want and
     # return code 200
     # i.e., return event, 200
     except:
         logger.error("No more messages found")
     logger.error("Could not find BP at index %d" % index)
-    return { "message": "Not Found"}, 404
+    return {"message": "Not Found"}, 404
 
 
 def get_place_Shipment(index):
-    hostname = "%s:%d" % (app_config["events"]["hostname"],app_config["events"]["port"])
+    hostname = "%s:%d" % (
+        app_config["events"]["hostname"], app_config["events"]["port"])
     client = KafkaClient(hosts=hostname)
     topic = client.topics[str.encode(app_config["events"]["topic"])]
     # Here we reset the offset on start so that we retrieve
-    # messages at the beginning of the message queue. 
+    # messages at the beginning of the message queue.
     # To prevent the for loop from blocking, we set the timeout to
     # 100ms. There is a risk that this loop never stops if the
     # index is large and messages are constantly being received!
-    consumer = topic.get_simple_consumer(reset_offset_on_start=True, 
-    consumer_timeout_ms=1000)
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True,
+                                         consumer_timeout_ms=1000)
     logger.info("Retrieving BP at index %d" % index)
+    counter = 0
     try:
         for msg in consumer:
             msg_str = msg.value.decode('utf-8')
             msg = json.loads(msg_str)
-    # Find the event at the index you want and 
+            if counter == index and msg['type'] == "shipping":
+                return msg, 200
+            counter += 1
+    # Find the event at the index you want and
     # return code 200
     # i.e., return event, 200
     except:
         logger.error("No more messages found")
     logger.error("Could not find BP at index %d" % index)
-    return { "message": "Not Found"}, 404
-    
+    return {"message": "Not Found"}, 404
 
-    
+
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yaml",strict_validation=True, validate_responses=True)
+app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
 if __name__ == "__main__":
     app.run(port=8110)
